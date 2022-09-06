9da31f45bdeb03b66c1289bb6724cd51c151349b
Sasha Kurach <sashakurach@ukr.net>
2022-09-06 21:30:53 +0300

    Move placeholder data to backend

--- frontend/components/activities/CompleteWithOptions.jsx
+++ frontend/components/activities/CompleteWithOptions.jsx
@@ -1,28 +1,16 @@
 import Question from "./Question";
+import {useEffect, useState} from "react";
 
 export default function CompleteWithOptions() {
-    const questions = [
-        {
-            "text": "Eva Hunt is a _ .",
-            "options": ["doctor", "historian", "writer"]
-        },
-        {
-            "text": "Eva Hunt went to look at the statues _.",
-            "options": ["yesterday", "last year", "last week"]
-        },
-        {
-            "text": "Some of the statues are _ meters tall.",
-            "options": ["10", "20", "12"]
-        },
-        {
-            "text": "The statues appeared between _.",
-            "options": ["1250 and 1500", "1200 and 1500", "1250 and 1300"]
-        },
-        {
-            "text": "The statues all have _",
-            "options": ["feet", "hands", "faces"]
-        }
-    ];
+    //
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() =>{
+        fetch('http://localhost:8000/api/greeting')
+            .then((response) => response.json())
+            .then(data => setQuestions(data))
+    }, [setQuestions])
+
 
     return <ul className="question-list">
         {questions.map(({text, options}, index) => <li className="question" key={index.toString()}><Question text={text} options={options} index={index + 1}/></li>)}
